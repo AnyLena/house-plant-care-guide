db4cc35c6933d41974a3480db63b71466cfba656
LenaBultmann <lena.bultmann@gmx.de>
2024-04-07 12:28:51 +0200

    initial commit

---
+++ src/App.jsx
@@ -0,0 +1,24 @@
+import Navigation from './components/Navigation';
+import Monstera from './components/Monstera';
+import HeroSection from './components/HeroSection';
+import Agave from './components/Agave';
+import BestSeller from './components/BestSeller';
+import IndoorPlants from './components/IndoorPLants';
+import HealthyHappy from './components/HealthyHappy';
+
+function App() {
+
+  return (
+    <main>
+      <Navigation />
+      <HeroSection />
+      <Monstera />
+      <Agave />
+      <BestSeller />
+      <IndoorPlants/>
+      <HealthyHappy/>
+    </main>
+  )
+}
+
+export default App
---
+++ src/components/Navigation.jsx
@@ -0,0 +1,38 @@
+import React from "react";
+import "../styles/navigation.css";
+import "../index.css";
+import { PiLeafLight } from "react-icons/pi";
+
+const Navigation = () => {
+  return (
+    <>
+      <nav id="hamnav">
+        <label for="hamburger">&#9776;</label>
+        <input type="checkbox" id="hamburger" />
+
+        <div id="hamitems">
+          <a href="#home">
+            <PiLeafLight /> Home
+          </a>
+          <a href="#featured">
+            <PiLeafLight /> Featured Plant
+          </a>
+          <a href="#new-arrivals">
+            <PiLeafLight /> New Arrivals
+          </a>
+          <a href="#best-sellers">
+            <PiLeafLight /> Best Sellers
+          </a>
+          <a href="#indoor-plants">
+            <PiLeafLight /> Indoor Plants
+          </a>
+          <a href="#healthy-happy">
+            <PiLeafLight /> Tipps
+          </a>
+        </div>
+      </nav>
+    </>
+  );
+};
+
+export default Navigation;
